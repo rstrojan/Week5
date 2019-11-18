4aababd9b0bde5ce6971ae5f8f6b796aee3d273c
rstrojan <32496785+rstrojan@users.noreply.github.com>
2019-11-17 16:44:22 -0800

    Update forms-demo.js

--- express-forms/forms-demo.js
+++ express-forms/forms-demo.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 34692);
+app.set('port', 34691);
 
 app.get('/',function(req,res){
   res.render('home');
