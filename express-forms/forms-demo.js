b19ec2d6149c7f66e32269c5151b9016efe7d16b
rstrojan <32496785+rstrojan@users.noreply.github.com>
2019-11-17 18:30:40 -0800

    Update forms-demo.js

--- express-forms/forms-demo.js
+++ express-forms/forms-demo.js
@@ -33,7 +33,7 @@ app.get('/get-loopback',function(req,res){
   res.render('get-loopback', context);
 });
 
-app.get('/get-loopback-improved',function(req,res){
+app.get('/suterr-returner',function(req,res){
   var qParams = [];
   for (var p in req.query){
     qParams.push({'name':p,'value':req.query[p]})
@@ -43,7 +43,7 @@ app.get('/get-loopback-improved',function(req,res){
   res.render('get-loopback-improved', context);
 });
 
-app.post('/post-loopback', function(req,res){
+app.post('/suterr-returner', function(req,res){
   var qParams = [];
   for (var p in req.body){
     qParams.push({'name':p,'value':req.body[p]})
