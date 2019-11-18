bd996859ab26c3a69fd5733b14fc97849798cd1f
rstrojan <32496785+rstrojan@users.noreply.github.com>
2019-11-17 18:07:34 -0800

    a

--- express-forms/forms-demo.js
+++ express-forms/forms-demo.js
@@ -17,6 +17,7 @@ app.get('/',function(req,res){
     qParams.push({'name':p,'value':req.query[p]})
   }
   var context = {};
+  context.reqType = 'GET';
   context.dataList = qParams;
   res.render('get-loopback-improved', context);
 });
